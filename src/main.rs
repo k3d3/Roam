2b3e74a99113d08feff1f62e638d4a3fa7939041
k3d3 <k3d3>
2017-03-29 21:06:22 -0400

    Add a bunch of documentation
    Move extern crates to main, rather than specifying in network_config
    Move subcommands to their own functions
    Make a test that's actually a test and not printing stuff out
    Gitignore IntelliJ files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+//! Roam is a simple, secure peer-to-peer VPN.
+//!
+//! Unlike a tool such as OpenVPN, Roam connects directly to each endpoint, rather than going
+//! through a central server.
+//!
+//! Every network is identified by a randomly generated shared secret, such as
+//!
+//! ```text
+//! ml0Pm4ie8ZL81DdFmft5x2rNfy2Xgl534CKs1ObBb-0
+//! ```
+//!
+//! This secret is actually the public side of a cryptographic key pair. With the secret key, a key
+//! pair looks like
+//!
+//! ```text
+//! ml0Pm4ie8ZL81DdFmft5x2rNfy2Xgl534CKs1ObBb-0:T4SW7Ap-VIBUzjgtLkis4WKIE3M2Ozd4m0PGbWkqdIeaXQ-biJ7xkvzUN0WZ-3nHas1_LZeCXnfgIqzU5sFv7Q
+//! ```
+//!
+//! This secret key gives you full control over the network, giving you the ability to change the
+//! general configuration, whitelist/blacklist/kick nodes, etc.
+//!
+//! If you only have the access key, however, you can only connect to the network.
+
 #![recursion_limit = "1024"]
 
 #[macro_use]
@@ -5,6 +28,9 @@ extern crate serde_derive;
 #[macro_use]
 extern crate error_chain;
 extern crate clap;
+extern crate serde;
+extern crate rand;
+extern crate crypto;
 
 mod network_config;
 
@@ -14,10 +40,11 @@ mod errors {
     error_chain!{}
 }
 
+/// Entry point into Roam. Basically just a Clap interface that calls one of the `command_*` functions.
 fn main() {
     let matches = App::new("Roam")
         .version("0.1")
-        .about("Direct P2P VPN")
+        .about("Simple, secure P2P VPN")
         .arg(Arg::with_name("ncurses").short("n").long("ncurses").help("Show an ncurses interface"))
         .subcommand(SubCommand::with_name("new").about("Create a new network"))
         .subcommand(SubCommand::with_name("connect").about("Connect to an existing network"))
@@ -29,23 +56,35 @@ fn main() {
         .get_matches();
 
     match matches.subcommand_name() {
-        Some("new") => {
-            match network_config::new_network_prompt() {
-                Ok(network) => {
-                    println!("Network is {:?}", network);
-                    network_config::save_network_config(network);
-                }
-                Err(err) => {
-                    println!("Error: {}", err);
-                    for e in err.iter().skip(1) {
-                        println!("Caused by: {}", e);
-                    }
-                }
-            }
-        }
-        Some("connect") => {
-            println!("Connect!");
-        }
+        Some("new") => command_new(),
+        Some("connect") => command_connect(),
+        Some("monitor") => command_monitor(),
         _ => {}
     }
+}
+
+/// Command to build a new network.
+fn command_new() {
+    match network_config::new_network_prompt() {
+        Ok(network) => {
+            println!("Network is {:?}", network);
+            //network_config::save_network_config(&network);
+        }
+        Err(err) => {
+            println!("Error: {}", err);
+            for e in err.iter().skip(1) {
+                println!("Caused by: {}", e);
+            }
+        }
+    }
+}
+
+/// Command to connect to an existing network.
+fn command_connect() {
+
+}
+
+/// Command to run an ncurses monitor a connected network.
+fn command_monitor() {
+
 }
--- src/network_config.rs
+++ src/network_config.rs
@@ -1,17 +1,17 @@
-extern crate serde;
+//! Module containing all functions and structures relating to the network configuration.
+
 extern crate serde_json;
 extern crate error_chain;
-extern crate rand;
 extern crate base64;
-extern crate crypto;
 
 use std::io;
 use std::result::Result as stdResult;
 use std::net::{IpAddr, Ipv4Addr};
 use std::io::prelude::*;
-use network_config::rand::Rng;
-use self::serde::{Serialize, Serializer, Deserialize, Deserializer};
-use self::crypto::ed25519;
+use std::str::FromStr;
+use rand::{self, Rng};
+use serde::{Serializer, Serialize, Deserialize, Deserializer};
+use crypto::ed25519;
 
 mod errors {
     error_chain!{}
@@ -19,45 +19,116 @@ mod errors {
 
 use errors::*;
 
-// A network key is an ed25519-derived keypair.
-// Knowledge of the public key allows you to join the network
-// Knowledge of the access key allows you to control/config the network
+/// The internal representation of a network configuration.
+#[derive(Debug, Serialize)]
+pub struct NetworkConfig {
+    /// Name of the network.
+    ///
+    /// This can be anything arbitrary, however if a node knows two of the same network name,
+    /// one of them might be appended with a number.
+    pub name: String,
+
+    /// Network key associated with this network.
+    pub key: NetworkKey,
+
+    /// Network address of the network's subnet.
+    pub network_addr: IpAddr,
+
+    /// Size of the network mask in CIDR representation.
+    pub cidr: u8,
+}
+
+/// A network key is used to connect to or control a network.
 #[derive(Debug)]
 pub struct NetworkKey {
+    /// The access key allows you to access the network.
     pub access_key: Vec<u8>,
+    /// The secret key allows you to control the network.
     pub secret_key: Option<Vec<u8>>,
 }
 
+impl NetworkConfig {
+    /// Convert a NetworkConfig to JSON format, to be saved as a config file.
+    pub fn to_json(&self) -> Result<String> {
+        serde_json::to_string_pretty(&self).chain_err(|| "Could not serialize network config")
+    }
+}
+
+impl ToString for NetworkKey {
+    fn to_string(&self) -> String {
+        let access_key_base64 = base64::encode_config(&self.access_key, base64::URL_SAFE_NO_PAD);
+        match self.secret_key {
+            Some(ref sec_key) => {
+                let secret_key_base64 = base64::encode_config(&sec_key[..], base64::URL_SAFE_NO_PAD);
+                {
+                    let mut mut_output_str = String::with_capacity(access_key_base64.len() + secret_key_base64.len() + 1);
+                    mut_output_str.push_str(&access_key_base64);
+                    mut_output_str.push(':');
+                    mut_output_str.push_str(&secret_key_base64);
+                    mut_output_str
+                }
+            }
+            _ => access_key_base64
+        }
+    }
+}
+
+impl FromStr for NetworkKey {
+    type Err = Error;
+    fn from_str(s: &str) -> stdResult<Self, Self::Err> {
+        let mut keys = s.split(':')
+            .flat_map(|key| base64::decode_config(key, base64::URL_SAFE_NO_PAD));
+        if let Some(access_key) = keys.next() {
+            Ok(NetworkKey {
+                access_key: access_key,
+                secret_key: keys.next()
+            })
+        } else {
+            Err("Failed to deserialize access key".into())
+        }
+    }
+}
+
 impl Serialize for NetworkKey {
+    /// Serialize a NetworkKey into a colon-separated string.
+    ///
+    /// If the network key contains a secret key, the output format will look like
+    ///
+    ///     <access_key>:<secret_key>
+    ///
+    /// If the network key does not contain a secret key, however, only the access
+    /// key will be returned.
     fn serialize<S>(&self, serializer: S) -> stdResult<S::Ok, S::Error>
         where S: Serializer
     {
         let access_key_base64 = base64::encode_config(&self.access_key, base64::URL_SAFE_NO_PAD);
-        if let Some(ref sec_key) = self.secret_key {
-            let secret_key_base64 = base64::encode_config(&sec_key[..], base64::URL_SAFE_NO_PAD);
-            let output_str = {
-                let mut mut_output_str = String::with_capacity(access_key_base64.len() + secret_key_base64.len() + 1);
-                mut_output_str.push_str(&access_key_base64);
-                mut_output_str.push(':');
-                mut_output_str.push_str(&secret_key_base64);
-                mut_output_str
-            };
-            serializer.serialize_str(&output_str)
-        } else {
-            serializer.serialize_str(&access_key_base64)
+        match self.secret_key {
+            Some(ref sec_key) => {
+                let secret_key_base64 = base64::encode_config(&sec_key[..], base64::URL_SAFE_NO_PAD);
+                let output_str = {
+                    let mut mut_output_str = String::with_capacity(access_key_base64.len() + secret_key_base64.len() + 1);
+                    mut_output_str.push_str(&access_key_base64);
+                    mut_output_str.push(':');
+                    mut_output_str.push_str(&secret_key_base64);
+                    mut_output_str
+                };
+                serializer.serialize_str(&output_str)
+            }
+            _ => serializer.serialize_str(&access_key_base64),
         }
 
     }
 }
 
 impl Deserialize for NetworkKey {
+    /// Deserialize a colon-separated string into a NetworkKey.
     fn deserialize<D>(deserializer: D) -> stdResult<Self, D::Error>
         where D: Deserializer
     {
-        use self::serde::de::Error;
+        use serde::de::Error;
 
         String::deserialize(deserializer).and_then(|string| {
-            let mut keys = string.split(":").flat_map(|key| base64::decode_config(key, base64::URL_SAFE_NO_PAD));
+            let mut keys = string.split(':').flat_map(|key| base64::decode_config(key, base64::URL_SAFE_NO_PAD));
             if let Some(access_key) = keys.next() {
                 Ok(NetworkKey {
                        access_key: access_key,
@@ -70,14 +141,7 @@ impl Deserialize for NetworkKey {
     }
 }
 
-#[derive(Debug, Serialize)]
-pub struct NetworkConfig {
-    pub name: String,
-    pub key: NetworkKey,
-    pub network_addr: IpAddr,
-    pub cidr: u8,
-}
-
+/// Generate an access and secret key pair.
 fn generate_secret_key() -> NetworkKey {
     let mut rng = rand::thread_rng();
 
@@ -93,11 +157,15 @@ fn generate_secret_key() -> NetworkKey {
     }
 }
 
-fn string_to_ip_cidr(input: &str) -> Result<Option<(IpAddr, u8)>> {
+/// Convert a string to an IP/CIDR pair.
+///
+/// Returns a Result for input errors, and an Option for empty input.
+/// The option can be used to set a default subnet if one is not provided.
+pub fn string_to_ip_cidr(input: &str) -> Result<Option<(IpAddr, u8)>> {
     if input.is_empty() {
         Ok(None)
     } else {
-        let mut input_parts = input.split("/");
+        let mut input_parts = input.split('/');
 
         let ip_addr_string = input_parts.next().ok_or("IP address not provided")?;
         let ip_addr: IpAddr = ip_addr_string.parse().chain_err(|| "Could not parse IP address")?;
@@ -112,7 +180,8 @@ fn string_to_ip_cidr(input: &str) -> Result<Option<(IpAddr, u8)>> {
     }
 }
 
-fn question_prompt(input: &str) -> Result<String> {
+/// Simple prompt function used by [new_network_prompt](fn.new_network_prompt.html).
+pub fn question_prompt(input: &str) -> Result<String> {
     let mut stdout = io::stdout();
 
     write!(&mut stdout, "\n{}\n> ", input).chain_err(|| "Unable to write to stdout")?;
@@ -123,6 +192,7 @@ fn question_prompt(input: &str) -> Result<String> {
     Ok(input.trim().into())
 }
 
+/// Prompt for the information needed to generate a network config.
 pub fn new_network_prompt() -> Result<NetworkConfig> {
     println!("To set up your network, we need to ask a few questions first.");
 
@@ -131,8 +201,9 @@ pub fn new_network_prompt() -> Result<NetworkConfig> {
         bail!("A network name needs to be provided.");
     }
 
-    let (ip_addr, cidr) =
-        string_to_ip_cidr(&question_prompt("What subnet should be used for this network? (or leave blank for 192.168.251.0/24)")?)?.unwrap_or((IpAddr::V4(Ipv4Addr::new(192, 168, 251, 0)), 24));
+    let ip_cidr = question_prompt("What subnet should be used for this network? (or leave blank for 192.168.251.0/24)")?;
+
+    let (ip_addr, cidr) = string_to_ip_cidr(&ip_cidr)?.unwrap_or((IpAddr::V4(Ipv4Addr::new(192, 168, 251, 0)), 24));
 
     let network_key = generate_secret_key();
 
@@ -144,46 +215,27 @@ pub fn new_network_prompt() -> Result<NetworkConfig> {
        })
 }
 
-pub fn save_network_config(config: NetworkConfig) -> Result<()> {
-    let config_json = serde_json::to_string_pretty(&config).chain_err(|| "Could not save network config")?;
-    println!("config json: {}", config_json);
-    Ok(())
-}
-
+#[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_generate_secret_key() {
-        let network_key = generate_secret_key();
-        println!("{:?}", network_key);
+    fn serialize_network_key() {
+        let access_input = NetworkKey{ access_key: vec![105, 199, 44], secret_key: None };
+        assert!(access_input.to_string() == "accs");
+
+        let secret_input = NetworkKey{ access_key: vec![105, 199, 44], secret_key: Some(vec![177, 202, 237]) };
+        assert!(secret_input.to_string() == "accs:scrt");
     }
 
     #[test]
-    fn split_key() {
-        let input = "mo_AiJm7OZRiM3U2r6S2lEelXNUWcKS3iP-fFF_bvYI:-scJXq0RfnDo3yFFvlYjGg87hM_7durzOwMKxIifWW6aj8CImbs5lGIzdTavpLaUR6Vc1RZwpLeI_58UX9u9gg";
-        let a = input.split(":");
-        let mut b = a.flat_map(|x| base64::decode_config(x, base64::URL_SAFE_NO_PAD).ok());
-        if let Some(x) = b.next() {
-            let net_key = NetworkKey {
-                access_key: x,
-                secret_key: b.next(),
-            };
-            println!("{:?}", net_key);
-        }
+    fn deserialize_network_key() {
+
     }
 
     #[test]
-    fn split_access_key() {
-        let input = "mo_AiJm7OZRiM3U2r6S2lEelXNUWcKS3iP-fFF_bvYI&*^%";
-        let a = input.split(":");
-        let mut b = a.flat_map(|x| base64::decode_config(x, base64::URL_SAFE_NO_PAD).ok());
-        if let Some(x) = b.next() {
-            let net_key = NetworkKey {
-                access_key: x,
-                secret_key: b.next(),
-            };
-            println!("{:?}", net_key);
-        }
+    fn blah_deserialize() {
+
     }
+
 }
